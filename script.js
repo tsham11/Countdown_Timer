e60e41a3f785bdbf317b2a232a203c4fedc7ece2
tsham11 <shamugiatamar22@gmail.com>
2023-10-11 21:17:12 +0400

    add files

---
+++ script.js
@@ -0,0 +1,34 @@
+// Set the launch date (replace with your desired date)
+const launchDate = new Date("2023-12-31T00:00:00").getTime();
+
+// Update the countdown timer every second
+const countdown = setInterval(function () {
+  const currentDate = new Date().getTime();
+  const timeRemaining = launchDate - currentDate;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+  document.getElementById("hours").innerHTML = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").innerHTML = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").innerHTML = seconds
+    .toString()
+    .padStart(2, "0");
+
+  if (timeRemaining <= 0) {
+    clearInterval(countdown);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
